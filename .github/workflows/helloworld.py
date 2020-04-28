45cc308edca841ab041447de90de7ade3ac22a5c
nikunj-agrawal <nikunj-agrawal@hotmail.com>
2020-04-28 21:23:08 +0530

    commit

--- .github/workflows/helloworld.py
+++
@@ -1,5 +0,0 @@
-print("hello world")
-#Sum and Multiply
-Numbers = [22,66,97]
-Sum = sum(Numbers)
-print(Sum*Sum*Sum)
---
+++ .github/workflows/oddeven.py
@@ -0,0 +1,17 @@
+for x in range(20):
+    if x%2==0:
+        print(x),
+    else:
+        print("ODD"),
+for x in range(20):
+    if x%2==1:
+        print(x),
+    else:
+        print("EVEN"),
+for x in range(20):
+    if x%2==0:
+        print("EVEN"),
+    elif x%2==1:
+        print("ODD"),
+    else:
+        ("INVALID"),
